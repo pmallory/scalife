4ff37f6565358efe5b92e9bb5151241c573267c0
Philip Mallory <philip@pmallory.com>
2017-03-07 13:33:55 -0800

    more test cases for NeighborCount

--- src/test/scala/life/LifeSuite.scala
+++ src/test/scala/life/LifeSuite.scala
@@ -16,5 +16,17 @@ class LifeSuite extends FunSuite {
   test("a cell in an empty generation has no neighbors") {
     assert(neighborCount((1,2), Set()) === 0)
   }
+  test("a cell is not its own neighbor") {
+    assert(neighborCount((1,2), Set((1,2))) === 0)
+  }
+  test("a cell with one neighbor") {
+    assert(neighborCount((1,2), Set((1,2), (2,2))) === 1)
+  }
+  test("a cell with eight neighbors") {
+    assert(neighborCount((1,1), Set((0,0), (0,1), (0,2),
+                                    (1,0), (1,1), (1,2),
+                                    (2,0), (2,1), (2,2)))
+           === 8)
+  }
 
 }
